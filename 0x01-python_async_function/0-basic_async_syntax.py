03de74fc45ad2365ca87af59854efa9670097fbb
Ojobumiche <bumiche@mail.com>
2023-10-10 05:53:40 -0700

    0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,17 +1,13 @@
+#!/usr/bin/env python3
+
 import asyncio
 from random import uniform
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''Asynchronous coroutine that waits for a random delay between 0 and max_delay (inclusive) seconds.'''
+    '''This is an asynchronous coroutine that waits for a random delay between 0 and max_delay (inclusive) seconds.'''
     random_delay = uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
-
-
-wait_random = __import__('0-basic_async_syntax').wait_random
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
 
 
