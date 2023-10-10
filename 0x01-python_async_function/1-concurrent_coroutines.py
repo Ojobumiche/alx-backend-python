3f5a027429e55e7680b8d2cc40edf7caa93d2fc8
Ojobumiche <bumiche@mail.com>
2023-10-10 03:26:40 -0700

    bug fixed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,37 +1,33 @@
-#!/usr/bin/env python3
-
-
 from typing import List
+import asyncio
+from random import uniform
 
 
-''' Import wait_random from the previous python file that you’ve
-    written and write an async routine called wait_n that takes
-    in 2 int arguments (in this order): n and max_delay. You will
-    spawn wait_random n times with the specified max_delay.
-
-    wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending order without using
-    sort() because of concurrency.
-'''
+async def wait_random(max_delay: int) -> float:
+    '''
+    Async function that waits for a random delay between 0 and max_delay (inclusive)
+    and then returns it as a float.
+    '''
+    random_delay = uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    ''' returns a sorted list of float numbers gotten randomly'''
-    wait_random = __import__('0-basic_async_syntax').wait_random
-
+    '''
+    Async function that spawns n instances of wait_random with the specified max_delay.
+    Returns a list of all the delays in ascending order.
+    '''
     delay_list = []
-    i = 0
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delay_list.append(delay)
 
-    while i < n:
-        delay_list.append(await wait_random(max_delay))
-        i += 1
-
-    return sorted(delay_list)
+    delay_list.sort()
+    return delay_list
 
 
 if __name__ == '__main__':
-    import asyncio
-
     print(asyncio.run(wait_n(5, 2)))
     print(asyncio.run(wait_n(7, 4)))
     print(asyncio.run(wait_n(10, 0)))
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,24 +1,10 @@
-#!/usr/bin/env python3
-
-
 import asyncio
 import time
-
-
-'''From the previous file, import wait_n into 2-measure_runtime.py.
-
-    Create a measure_time function with integers n and max_delay as arguments
-    that measures the total execution time for wait_n(n, max_delay), and
-    returns total_time / n. Your function should return a float.
-
-    Use the time module to measure an approximate elapsed time.
-'''
+from concurrent_coroutines import wait_n  # Correct the import statement
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    '''returnsthe total time coroutines took to completion'''
-    wait_n = __import__('1-concurrent_coroutines').wait_n
-
+    '''Returns the total time coroutines took to complete'''
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     total_time = time.time() - start_time
